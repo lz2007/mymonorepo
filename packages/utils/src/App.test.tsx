5756c662788c6d57fb466726549075d40ca93e04
liangzhu <liangzhu7904@dingtalk.com>
2023-06-05 16:49:17 +0800

    del no-use

--- packages/utils/src/App.test.tsx
+++ packages/utils/src/App.test.tsx
@@ -1,12 +1,11 @@
 import {describe, expect, test } from 'vitest'
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import React from "react";
 
 describe("Accordion test", () => {
   test("should show title all the time", () => {
     expect(true).toBe(true)
       render(<App></App>);
-      expect(screen.getByText(/Edit/i)).toBeInTheDocument()
+      expect(screen.getByText(/Edit/i)).toBeDefined()
   })
 })
